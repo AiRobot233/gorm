35e0cf3087e2087213c5579523c0410c40d87769
HP <2274642118@qq.com>
2023-03-28 15:11:47 +0800

    优化代码 env设置gin_mode

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "gin/routes"
+import (
+	"gin/routes"
+	"gin/utils"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	if utils.GetEnvData("GIN_MODE") == "release" {
+		gin.SetMode(gin.ReleaseMode)
+	}
 	r := routes.Routes()
 	_ = r.Run(":9501")
 }
--- routes/route.go
+++ routes/route.go
@@ -10,7 +10,14 @@ import (
 )
 
 func Routes() *gin.Engine {
-	r := gin.Default()
+	r := gin.New()
+	// 全局中间件
+	// Logger 中间件将日志写入 gin.DefaultWriter，即使你将 GIN_MODE 设置为 release。
+	// By default gin.DefaultWriter = os.Stdout
+	r.Use(gin.Logger())
+	// Recovery 中间件会 recover 任何 panic。如果有 panic 的话，会写入 500。
+	r.Use(gin.Recovery())
+
 	r.Static("/storage", "./storage") //文件访问配置地址
 	r.Use(middleware.LogAuth())
 
