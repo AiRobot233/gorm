5350b7b684a3e6a298083a951aeb5e55232db931
HP <2274642118@qq.com>
2023-03-28 14:04:55 +0800

    优化日志

--- middleware/LogAuth.go
+++ middleware/LogAuth.go
@@ -1,67 +1,38 @@
 package middleware
 
 import (
-	"bytes"
-	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"os"
 	"time"
 )
 
-type jsonResponseWriter struct {
-	gin.ResponseWriter
-	body *bytes.Buffer
-}
-
-func (w jsonResponseWriter) Write(data []byte) (int, error) {
-	w.body.Write(data)
-	return w.ResponseWriter.Write(data)
-}
-
-func (w jsonResponseWriter) Body() []byte {
-	return w.body.Bytes()
-}
-
 func LogAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// 创建自定义的 ResponseWriter 对象
-		w := &jsonResponseWriter{
-			ResponseWriter: c.Writer,
-			body:           bytes.NewBuffer([]byte{}),
-		}
-		// 替换 Gin 上下文的 Writer 字段为自定义的 ResponseWriter 对象
-		c.Writer = w
-		// 创建一个新的 Logrus 日志实例
-		logger := logrus.New()
-		// 设置日志输出格式
-		logger.SetFormatter(&logrus.JSONFormatter{TimestampFormat: time.DateTime})
-		file, _ := os.OpenFile(build(), os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
-		// 将日志输出到文件中
-		logger.SetOutput(file)
-		// 记录请求信息
-		logger.WithFields(logrus.Fields{
-			"method": c.Request.Method,
-			"url":    c.Request.URL.String(),
-			"ip":     c.ClientIP(),
-		}).Info("request")
-		// 记录响应信息
+		// 请求开始时间
+		startTime := time.Now()
 		c.Next()
-		// 从自定义的 ResponseWriter 对象中读取响应体
-		responseBody := w.Body()
-		// 解析 JSON 数据
-		var responseMap map[string]any
-		err := json.Unmarshal(responseBody, &responseMap)
-		if err != nil {
-			return
-		}
-		if responseMap["error"].(float64) > 0 {
+		// 请求结束时间
+		endTime := time.Now()
+		// 请求响应时间
+		latencyTime := endTime.Sub(startTime)
+		go func() {
+			// 创建一个新的 Logrus 日志实例
+			logger := logrus.New()
+			// 设置日志输出格式
+			logger.SetFormatter(&logrus.JSONFormatter{TimestampFormat: time.DateTime})
+			file, _ := os.OpenFile(build(), os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
+			// 将日志输出到文件中
+			logger.SetOutput(file)
 			logger.WithFields(logrus.Fields{
-				"error":   1,
-				"status":  c.Writer.Status(),
-				"message": responseMap["message"],
-			}).Info("response")
-		}
+				"client_ip":  c.ClientIP(),
+				"method":     c.Request.Method,
+				"status":     c.Writer.Status(),
+				"latency":    latencyTime.Milliseconds(),
+				"user_agent": c.Request.UserAgent(),
+				"path":       c.Request.URL.String(),
+			}).Info("ask")
+		}()
 	}
 }
 
--- services/admin/UserService.go
+++ services/admin/UserService.go
@@ -12,7 +12,7 @@ func UserList(page string, pageSize string, params model.UserSearch) (bool, any)
 	var users []model.User //定义表结构
 	var count int64
 	db.Table("user").Scopes(model.UserSearchFunc(params)).Count(&count)
-	result := db.Debug().Table("user").Preload("Role", func(db *gorm.DB) *gorm.DB {
+	result := db.Table("user").Preload("Role", func(db *gorm.DB) *gorm.DB {
 		return db.Select([]string{"id", "name"})
 	}).Select([]string{"id", "name", "phone", "role_id", "status", "created_at"}).Scopes(model.UserSearchFunc(params)).Scopes(model.Paginate(page, pageSize)).Find(&users)
 	return utils.R(result, utils.P(users, count))
